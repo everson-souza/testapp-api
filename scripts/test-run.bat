d3157a56afeacac9309f206ceeb54f14ac2223dc
sysop-weblakes <79984869+sysop-weblakes@users.noreply.github.com>
2021-10-07 11:54:15 -0700

    Update test-run.bat file

--- scripts/test-run.bat
+++ scripts/test-run.bat
@@ -1,11 +1,11 @@
 set rootPath=C:\Users\QAOperator\Documents\GitHub
+set testappPath=C:\inetpub\wwwroot
 
 ECHO STARTING
 ::PUBLISHING TO TAURIA CHANNEL
 ECHO PUBLISHING TO TAURIA CHANNEL
 cd C:\util
 curl.exe --data "Started test run on "%2" browser" "https://weblakes.zebu.io/mail_webhook/post?token="%3
-
 
 ::PULL CHANGES ON CYPRESS PROJECT
 ECHO ===== PULL CHANGES ON CYPRESS PROJECT =====
@@ -19,14 +19,6 @@ git fetch origin
 ::Pull
 git pull
 
-ECHO Updating Testapp
-::Go to testapp folder
-cd %rootPath%\testapp
-::Fetch
-git fetch origin
-::Pull
-git pull
-
 ::PREPARE RESULTS FOLDER
 ECHO ===== PREPARE RESULTS FOLDER =====
 
@@ -34,7 +26,7 @@ ECHO ===== PREPARE RESULTS FOLDER =====
 MD "%TEMP%\$_.dummy"&&ROBOCOPY "%TEMP%\$_.dummy" "%rootPath%\BOEM_Tests\%1\allure-results" /E /XD allure-report /PURGE>NUL 2>&1&RD "%TEMP%\$_.dummy"
 
 :: Copy history folder
-ROBOCOPY "%rootPath%\testapp\public\projects\%1\%2\allure-report\history" "%rootPath%\BOEM_Tests\%1\allure-results\history" /mir
+ROBOCOPY "%testappPath%\projects\%1\%2\allure-report\history" "%rootPath%\BOEM_Tests\%1\allure-results\history" /mir
 
 :: START TEST RUN
 ECHO ===== START TEST RUN =====
@@ -54,18 +46,7 @@ call allure generate %rootPath%\BOEM_Tests\%1\allure-results --clean -o allure-r
 
 ECHO Copy files to testapp
 :: Copy files to testapp
-ROBOCOPY "%rootPath%\BOEM_Tests\%1\allure-results\allure-report" "%rootPath%\testapp\public\projects\%1\%2\allure-report" /mir 
-
-::PUSH CHANGES TO TESTAPP
-ECHO ===== PUSH CHANGES TO TESTAPP =====
-::Push changes to testapp
-cd %rootPath%\testapp
-:: add files to commit
-git add --all
-:: commit changes
-git commit -m "Updated %1 - %2"
-::Push
-git push
+ROBOCOPY "%rootPath%\BOEM_Tests\%1\allure-results\allure-report" "%testappPath%\projects\%1\%2\allure-report" /mir
 
 ::PUBLISHING TO TAURIA CHANNEL
 ECHO PUBLISHING TO TAURIA CHANNEL
